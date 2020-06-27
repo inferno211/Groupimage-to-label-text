71cf831b56e7dce072801abaac9174ab7a1f8e51
Eldenroot <778890+Eldenroot@users.noreply.github.com>
2020-06-27 16:10:13 +0200

    Language corrections

--- inc/languages/english/admin/labelrank_group.lang.php
+++ inc/languages/english/admin/labelrank_group.lang.php
@@ -2,21 +2,21 @@
 $l['show_other_group_title'] = "Show additional groups";
 $l['show_other_group_descr'] = "Show additional groups?";
 
-$l['agi_descr'] = "Change rank images to label text";
+$l['agi_descr'] = "Transform rank images to label text";
 $l['setting_description'] = "Settings of plugin \"Groupimage to label text\".";
 
-$l['postbit_title'] = "Turn on in theards";
-$l['postbit_descr'] = "Turn on label texts in theards and posts?";
+$l['postbit_title'] = "Turn on in threads";
+$l['postbit_descr'] = "Turn on label texts in threads and posts?";
 
-$l['profile_title'] = "Turn on in profile";
+$l['profile_title'] = "Turn on in a user profile";
 $l['profile_descr'] = "Turn on label texts in users profile?";
 
 $l['usercp_title'] = "Turn on in UCP";
 $l['usercp_descr'] = "Turn on label texts in UCP?";
 
-$l['memberlist_title'] = "Turn on in memberlist";
-$l['memberlist_descr'] = "Turn on label texts in memberlist?";
+$l['memberlist_title'] = "Turn on in a memberlist";
+$l['memberlist_descr'] = "Turn on label texts in a memberlist?";
 
-$l['read_pw_title'] = "Turn on in private message";
-$l['read_pw_descr'] = "Turn on label texts in private messages?";
+$l['read_pw_title'] = "Turn on in a private message";
+$l['read_pw_descr'] = "Turn on label texts in a private messages?";
 ?>
